4fa6cf1d7304b7f0e39dc48e55ab92fd58c8dcfe
slamchillz <mendyslam@gmail.com>
2023-02-02 18:58:43 +0100

    doc string reduction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,17 +10,8 @@ def filter_datum(
     fields: List[str], redaction: str,
     message: str, seperator: str
 ) -> str:
-    """
-    Filters message by replacing each value in fields with redaction
-    Args:
-        fields (List): a list of strings representing all fields to obfuscate
-        redaction (str): representing by what the field will be obfuscated
-        message (str):  a string representing the log line
-        seperator (str): character separating all fields in log `message` line
-    Returns:
-        message (str): the filtered `message` log line
-    """
+    """Filters message by replacing each value in fields with redaction"""
     for key in fields:
-        pattern = r'({0}=)([^{1}]*)({1}?)'.format(key, seperator)
+        pattern = r'({0}=)([^{1}]*)({1})'.format(key, seperator)
         message = re.sub(pattern, r'\1{}\3'.format(redaction), message)
     return message
