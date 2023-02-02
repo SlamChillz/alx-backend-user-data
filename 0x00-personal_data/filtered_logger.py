e9ae3502295371585a160d0100e30841cd25105c
slamchillz <mendyslam@gmail.com>
2023-02-02 23:20:41 +0100

    custom log formatter class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Defines a function that filters logs
 """
 import re
+import logging
 from typing import List
 
 
@@ -17,3 +18,25 @@ def filter_datum(
         pattern = r'({0}=)[^{1}]*({1})'.format(key, separator)
         message = re.sub(pattern, r'\1{}\2'.format(redaction), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Instantiation method, sets fields for each instance
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the LogRecord instance
+        """
+        log = super(RedactingFormatter, self).format(record=record)
+        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
