2e29633fa2e06663b5f946629d3daad63a445696
slamchillz <mendyslam@gmail.com>
2023-02-03 01:55:48 +0100

    read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -52,10 +52,11 @@ def get_logger() -> logging.Logger:
     """
     Creates and configures a logger
     """
-    logger = logging.getLogger('user_data').setLevel(logging.INFO)
-    logger.propagate = False
+    logger = logging.getLogger('user_data')
     handler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
     logger.addHandler(handler)
     return logger
 
@@ -71,3 +72,27 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         password=os.getenv('PERSONAL_DATA_DB_PASSWORD')
     )
     return connector
+
+
+def main() -> None:
+    """
+    Log database users
+    """
+    db = get_db()
+    logger = get_logger()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    for row in rows:
+        msg = (
+            "name={}; email={}; phone={}; ssn={}; "
+            "password={}; ip={}; last_login={}; user_agent={};"
+        ).format(
+            row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+        logger.info(msg)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
