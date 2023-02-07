02d21308beea766d963fd978f0f1ce8f34f941e0
slamchillz <mendyslam@gmail.com>
2023-02-07 14:32:06 +0100

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized[/]
+    Returns:
+      - an unauthorized error message
+    """
+    abort(401)
