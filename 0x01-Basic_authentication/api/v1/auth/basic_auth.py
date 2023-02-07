cb19044f56cdef6433f581891ec7f47dc84912c9
slamchillz <mendyslam@gmail.com>
2023-02-07 18:08:38 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """
 Defines a BasicAuth class that inherits from Auth class
 """
+import base64
+
 from api.v1.auth.auth import Auth
 
 
@@ -22,3 +24,20 @@ class BasicAuth(Auth):
         ):
             return None
         return ''.join(authorization_header.split('Basic ')[1:])
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        Base64 encode authorization_header
+        """
+        if (
+            base64_authorization_header is None
+            or type(base64_authorization_header) != str
+        ):
+            return None
+        try:
+            encoded = base64.b64decode(base64_authorization_header)
+        except Exception:
+            return None
+        return encoded.decode('utf-8')
