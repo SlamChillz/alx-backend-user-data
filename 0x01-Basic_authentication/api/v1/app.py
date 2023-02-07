94b822d79e784d4546d79e65498769b4dadf9dc8
slamchillz <mendyslam@gmail.com>
2023-02-07 16:47:27 +0100

    getting started with basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,7 +16,11 @@ auth = None
 
 auth_type = getenv('AUTH_TYPE')
 
-if auth_type == 'auth':
+
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Defines a BasicAuth class that inherits from Auth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Basic Authentication class implementation
+    """
+    pass
