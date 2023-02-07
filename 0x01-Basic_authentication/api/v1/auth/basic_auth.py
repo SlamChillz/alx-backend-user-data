847c68272ddd15b4992d9096b8ee7fea4a8f3981
slamchillz <mendyslam@gmail.com>
2023-02-07 20:07:38 +0100

    Get user object from credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,10 @@
 Defines a BasicAuth class that inherits from Auth class
 """
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
 
 from api.v1.auth.auth import Auth
+from api.v1.views.users import User
 
 
 class BasicAuth(Auth):
@@ -56,3 +57,22 @@ class BasicAuth(Auth):
         ):
             return None, None
         return tuple(decoded_base64_authorization_header.split(':')[:2])
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """
+        Gets the User instance based on given email and password
+        """
+        if (
+            user_email is None or type(user_email) != str
+            or user_pwd is None or type(user_pwd) != str
+        ):
+            return None
+        User.load_from_file()
+        if User.count() > 0:
+            users = User.search({'email': user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        return None
